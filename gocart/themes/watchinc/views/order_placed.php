9c45e7f3d80891054676b0b5e72ed2c2a0659d44
Gusti Azis <gooazs@gmail.com>
2016-01-24 22:54:59 +0700

    Update Another Page

--- gocart/themes/watchinc/views/order_placed.php
+++ gocart/themes/watchinc/views/order_placed.php
@@ -1,74 +1,71 @@
-<?php include('header.php'); ?>
+<?php
+	include('header.php');
+?>
 
-<div class="product">
-	<div class="container" style="padding-top: 25px;">
-		<div class="def-temp">
-			<div class="arrow-steps">
-				<div class="row">
-					<div class="col-xs-3">
-						<div class="arrow-step">
-							<p>CART</p>
+			<div class="checkout">
+				<div class="container">
+					<div class="def-temp">
+						<div class="arrow-steps">
+							<div class="row">
+								<div class="col-xs-3">
+									<div class="arrow-step">
+										<p>CART</p>
+									</div>
+								</div>
+								<div class="col-xs-3">
+									<div class="arrow-step">
+										<p>CHECKOUT</p>
+									</div>
+								</div>
+								<div class="col-xs-3">
+									<div class="arrow-step">
+										<p>CONFIRM ORDER</p>
+									</div>
+								</div>
+								<div class="col-xs-3">
+									<div class="arrow-step active">
+										<p>THANK YOU</p>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="col-xs-3">
-						<div class="arrow-step">
-							<p>CHECKOUT</p>
-						</div>
-					</div>
-					<div class="col-xs-3">
-						<div class="arrow-step">
-							<p>CONFIRM ORDER</p>
-						</div>
-					</div>
-					<div class="col-xs-3">
-						<div class="arrow-step active">
-							<p>THANK YOU</p>
+						<div class="row">
+							<div class="col-sm-2"></div>
+							<div class="col-sm-8">
+								<div class="box text-center">
+									<h3 class="title"><span class="orange">THANK</span> YOU</h3>
+										<div class="words">
+											<h4 class="text-uppercase">Payment for your order was successfully processed</h4><br>
+											<p>Your Order Number : <b><?php echo $order_id;?></b></p>
+											<p>Payment Method : <b><?php echo $payment['details']; ?></b></p>
+												<?php
+													if ($payment['details'] == "Paypal Express") {
+												?>
+													<p>Amount : $<?php echo $payment['total']; ?> USD</p>
+													<p>Thankyou for your shopping with Scarlet</p>
+													<p>You can review this order and download your invoice from the "<b>Order history</b>" section of your account by clicking "<a href="<?php echo base_url('secure/my_account');?>">My account</a>" on our Website</p>
+												<?php
+													} else {
+												?>
+													<p>Amount: <?php echo format_currency($this->go_cart->total()); ?></p>
+													<p>Please transfer your payment to : <b> BCA -  5810332893</b> / <b>Mandiri -  1680000137669</b></p>
+													<br/><br/>
+													<p>
+														<i>You have a maximum of 2 days to pay and confirm your payment.</i><br>
+														<i>Automatic cancellation will take place if no payment confirmation received.</i>
+													</p>
+												<?php
+													}
+												?>
+										</div>
+								</div>
+							</div>
+							<div class="col-sm-2"></div>
 						</div>
 					</div>
 				</div>
 			</div>
 
-			<center>
-				<p style="font: bold 16px georgia">Hi <?php echo $customer['firstname'];?> <?php echo $customer['lastname'];?></p>
-				<br/><br/>
-				<p style="font: bold 18px georgia; text-transform: uppercase">
-					<strong>Payment for your order was successfully processed</strong>
-				</p>
-				<br/><br/>
-				<p style="font: bold 13px georgia">Your Order Number:  <span style="color: #5b5b5b;"><?php echo $order_id;?></span></p>
-				<p style="font: bold 13px georgia">Payment Method: <?php echo $payment['details']; ?></p>
-
-			<?php 
-				if($payment['details'] == "Paypal Express"){
-			?>
-				<p style="font: bold 13px georgia">Amount: $<?php echo $payment['total']; ?> USD</p>
-				<br/><br/>
-				<p style="font: bold 13px georgia">Thankyou for your shopping with Scarlet</p>
-				<br/><br/>
-				<p style="font: italic 13px georgia">You can review this order and download your invoice from the "<span style="color: #5b5b5b;">Order history</span>" section of your account by clicking "<a href="<?php echo base_url('secure/my_account');?>" style="color: #5b5b5b;">My account</a>" on our Website</p>
-			<?php
-				} else {
-			?>
-				<p style="font: bold 13px georgia">Amount: <?php echo format_currency($this->go_cart->total()); ?></p>
-				<br/><br/>
-				<p style="font: bold 13px georgia">Please transfer your payment to:
-					<br/><br/>
-					<span style="color: #5b5b5b;"> BCA -  5810332893</span> <br/>
-					<span style="color: #5b5b5b;">Mandiri -  1680000137669</span>
-				</p>
-				<br/><br/>
-				<p style="font: italic 13px georgia" class="red">
-					You have a maximum of 2 days to pay and confirm your payment. <br/>
-					Automatic cancellation will take place if no payment confirmation received
-				</p>
-			<?php
-				}
-			?>
-			</center>
-
-		</div>
-	</div>
-</div>
-
-
-<?php include('footer.php');
+<?php
+	include('footer.php');
+?>
--- gocart/themes/watchinc/views/view_cart.php
+++ gocart/themes/watchinc/views/view_cart.php
@@ -29,13 +29,28 @@
 				</div>
 			</div>
 			<div class="content">
-				<div class="view-cart">
-					<?php echo form_open('cart/update_cart', array('id'=>'update_cart_form'));?>
-						<div class="row">
-							<div class="col-xs-12">
-								<?php if ($this->go_cart->total_items()==0):?>
-									<div class="message">There are no products in your cart!</div>
-								<?php else: ?>
+				<?php echo form_open('cart/update_cart', array('id'=>'update_cart_form'));?>
+					<?php if ($this->go_cart->total_items()==0):?>
+						<div class="empty-cart">
+							<div class="row">
+								<div class="col-xs-1"></div>
+									<div class="col-xs-6">
+									<div class="empty">
+										<h2>Your Shopping Bag is Empty</h2>
+										<p>If items in your shopping bag are missing, try signing in to your account to view them.</p>
+										<a href="../" type="button" class="btn btn-orange">GO SHOPPING</a>
+									</div>
+								</div>
+								<div class="col-xs-4">
+									<img class="img-responsive center-block" src="<?php echo base_url('/images/watchinc/cart-empty.png');?>">
+								</div>
+								<div class="col-xs-1"></div>
+							</div>
+						</div>
+					<?php else: ?>
+						<div class="view-cart">
+							<div class="row">
+								<div class="col-xs-12">
 									<div class="panel">
 										<div class="panel-heading">
 											<div class="row">
@@ -106,65 +121,65 @@
 										</div>
 									</div>
 								</div>
-							<?php endif;?>
-						</div>
-						<div class="row">
-							<div class="col-sm-6">
-								<div class="coupon">
-									<div class="panel panel-as-table">
-										<h3 class="title"><span class="orange">HAVE</span> A COUPON?</h3>
-										<!-- <input type="text" class="input2" name="coupon_code">
-										<input type="submit" class="button2" value="apply"> -->
-										<div class="form-inline">
-											<div class="form-group">
-												<label for="coupon-code" class="sr-only">Coupon Code</label>
-												<input type="text" class="form-control" id="coupon-code" name="coupon_code" placeholder="Coupon Code">
-											</div>
-											<button type="submit" class="btn btn-orange">APPLY</button>
-										</div>
-									</div>
-								</div>
 							</div>
-							<div class="col-sm-6">
-								<div class="total">
-									<div class="panel panel-as-table">
-										<h3 class="title"><span class="orange">CART</span> TOTAL</h3>
-										<div class="row">
-											<div class="col-sm-6">
-												<span>CART SUBTOTAL</span>
-											</div>
-											<div class="col-sm-6 text-right">
-												<?php echo format_currency($this->go_cart->subtotal());?>
-											</div>
-											<div class="col-sm-6">
-												<span>SHIPPING</span>
-											</div>
-											<div class="col-sm-6 text-right">
-												<?php echo format_currency($this->go_cart->shipping_cost());?>
-											</div>
-											<div class="col-sm-6">
-												<span>VOUCHER</span>
-											</div>
-											<div class="col-sm-6 text-right">
-												<?php echo format_currency(0-$this->go_cart->coupon_discount());   ?>
-											</div>
-											<div class="col-sm-6">
-												<span>ORDER TOTAL</span>
-											</div>
-											<div class="col-sm-6 text-right">
-												<?php echo format_currency($this->go_cart->total());?>
-											</div>
-											<div class="col-sm-12">
-												<input id="redirect_path" type="hidden" name="redirect" value=""/>
-												<br/><button class="btn btn-black pull-right" onClick="$('#redirect_path').val('checkout');">PROCEED TO CHECKOUT</button>
+							<div class="row">
+								<div class="col-sm-6">
+									<div class="coupon">
+										<div class="panel panel-as-table">
+											<h3 class="title"><span class="orange">HAVE</span> A COUPON?</h3>
+											<!-- <input type="text" class="input2" name="coupon_code">
+											<input type="submit" class="button2" value="apply"> -->
+											<div class="form-inline">
+												<div class="form-group">
+													<label for="coupon-code" class="sr-only">Coupon Code</label>
+													<input type="text" class="form-control" id="coupon-code" name="coupon_code" placeholder="Coupon Code">
+												</div>
+												<button type="submit" class="btn btn-orange">APPLY</button>
 											</div>
 										</div>
 									</div>
 								</div>
+								<div class="col-sm-6">
+									<div class="total">
+										<div class="panel panel-as-table">
+											<h3 class="title"><span class="orange">CART</span> TOTAL</h3>
+											<div class="row">
+												<div class="col-sm-6">
+													<span>CART SUBTOTAL</span>
+												</div>
+												<div class="col-sm-6 text-right">
+													<?php echo format_currency($this->go_cart->subtotal());?>
+												</div>
+												<div class="col-sm-6">
+													<span>SHIPPING</span>
+												</div>
+												<div class="col-sm-6 text-right">
+													<?php echo format_currency($this->go_cart->shipping_cost());?>
+												</div>
+												<div class="col-sm-6">
+													<span>VOUCHER</span>
+												</div>
+												<div class="col-sm-6 text-right">
+													<?php echo format_currency(0-$this->go_cart->coupon_discount());   ?>
+												</div>
+												<div class="col-sm-6">
+													<span>ORDER TOTAL</span>
+												</div>
+												<div class="col-sm-6 text-right">
+													<?php echo format_currency($this->go_cart->total());?>
+												</div>
+												<div class="col-sm-12">
+													<input id="redirect_path" type="hidden" name="redirect" value=""/>
+													<br/><button class="btn btn-black pull-right" onClick="$('#redirect_path').val('checkout');">PROCEED TO CHECKOUT</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
-					</form>
-				</div>
+					<?php endif;?>
+				</form>
 			</div>
 		</div>
 	</div>
